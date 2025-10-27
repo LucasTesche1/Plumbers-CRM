f8775f521294cf229c5446344a7c601e5b2d8502
LucasTesche1 <lucas@yujilive.com.br>
2025-10-27 13:27:57 -0300

    'image-added-to-main-page'

---
+++ force-app/main/default/lwc/imageComponent/imageComponent.js
@@ -0,0 +1,7 @@
+import { LightningElement } from 'lwc';
+
+import minhaImagem from '@salesforce/resourceUrl/img';
+
+export default class ImagemCentralizada extends LightningElement {
+    imagem = minhaImagem;
+}
